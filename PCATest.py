60ab57a0568a7a800c50f91d554b1639445253f7
L3RiZ <107563807+stvytrnw@users.noreply.github.com>
2023-11-16 14:21:27 +0100

    Update PCATest.py

--- PCATest.py
+++ PCATest.py
@@ -8,11 +8,12 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import svm
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
 from sklearn.metrics import r2_score
 from sklearn.neural_network import MLPClassifier
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
 
 df = pd.read_csv("emails.csv")
 
@@ -78,12 +79,61 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # print('Std score: {}'.format(scores.std()))
 # print()
 
-print('NEURAL NETWORK')
-model = MLPClassifier(random_state=42, max_iter=10000, hidden_layer_sizes=[1000,1000])
-print('Train R2: {}'.format(r2_score(y_train, model.fit(X_train, y_train).predict(X_train))))
-print('Test R2: {}'.format(r2_score(y_test, model.fit(X_train, y_train).predict(X_test))))
-scores = cross_val_score(model, X_train, y_train, cv=5, scoring='r2')
-print('Scores: {}'.format(scores))
-print('Mean score: {}'.format(scores.mean()))
-print('Std score: {}'.format(scores.std()))
-print()
+# print('NEURAL NETWORK')
+# model = MLPClassifier(random_state=42, max_iter=10000, hidden_layer_sizes=[1000,1000])
+# print('Train R2: {}'.format(r2_score(y_train, model.fit(X_train, y_train).predict(X_train))))
+# print('Test R2: {}'.format(r2_score(y_test, model.fit(X_train, y_train).predict(X_test))))
+# scores = cross_val_score(model, X_train, y_train, cv=5, scoring='r2')
+# print('Scores: {}'.format(scores))
+# print('Mean score: {}'.format(scores.mean()))
+# print('Std score: {}'.format(scores.std()))
+# print()
+
+# Define and compile the neural network model
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(100, activation='relu', input_shape=(X.shape[1],)),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(100, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer=tf.keras.optimizers.Adam(0.005), loss='binary_crossentropy', metrics=['accuracy'])
+
+# Perform cross-validation
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+def plot_history(history, fold):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+    ax1.plot(history.history['loss'], label='Training Loss')
+    ax1.plot(history.history['val_loss'], label='Validation Loss')
+    ax1.set_xlabel('Epoch')
+    ax1.set_ylabel('Binary Crossentropy')
+    ax1.legend()
+    ax1.grid(True)
+
+    ax2.plot(history.history['accuracy'], label='Training Accuracy')
+    ax2.plot(history.history['val_accuracy'], label='Validation Accuracy')
+    ax2.set_xlabel('Epoch')
+    ax2.set_ylabel('Accuracy')
+    ax2.legend()
+    ax2.grid(True)
+
+    fig.suptitle(f'Fold {fold + 1}')
+    plt.show()
+
+for fold, (train_index, val_index) in enumerate(cv.split(X, y)):
+    X_train, X_val = X[train_index], X[val_index]
+    y_train, y_val = y[train_index], y[val_index]
+
+    # Train the model
+    history = model.fit(X_train, y_train, epochs=100, batch_size=128, validation_data=(X_val, y_val), verbose=0)
+
+    # Plot the training history for each fold
+    plot_history(history, fold)
+
+# Evaluate the model on the entire test set
+test_preds = model.predict(X)
+test_preds_binary = np.round(test_preds).flatten()
+test_accuracy = accuracy_score(y, test_preds_binary)
+print('Test Accuracy:', test_accuracy)
