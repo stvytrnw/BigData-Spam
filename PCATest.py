ef2f8e130240570e68800f34ea54b1ca845e918b
L3RiZ <107563807+stvytrnw@users.noreply.github.com>
2023-11-16 14:26:59 +0100

    Update PCATest.py

--- PCATest.py
+++ PCATest.py
@@ -26,6 +26,9 @@ X = scaler.fit_transform(X)
 model = PCA(n_components=0.99)
 model.fit(X)
 X = model.transform(X)
+print(model.explained_variance_ratio_) #AUF JEDEN FALL IN PRÄSENTATION (LINEARES PROBLEM)
+plt.plot(range(1, 30), np.cumsum(model.explained_variance_ratio_))
+plt.show()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
 
@@ -90,50 +93,50 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # print()
 
 # Define and compile the neural network model
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(100, activation='relu', input_shape=(X.shape[1],)),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(100, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Dense(100, activation='relu', input_shape=(X.shape[1],)),
+#     tf.keras.layers.Dropout(0.2),
+#     tf.keras.layers.Dense(100, activation='relu'),
+#     tf.keras.layers.Dropout(0.2),
+#     tf.keras.layers.Dense(1, activation='sigmoid')
+# ])
 
-model.compile(optimizer=tf.keras.optimizers.Adam(0.005), loss='binary_crossentropy', metrics=['accuracy'])
+# model.compile(optimizer=tf.keras.optimizers.Adam(0.005), loss='binary_crossentropy', metrics=['accuracy'])
 
-# Perform cross-validation
-cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+# # Perform cross-validation
+# cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
-def plot_history(history, fold):
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
-    ax1.plot(history.history['loss'], label='Training Loss')
-    ax1.plot(history.history['val_loss'], label='Validation Loss')
-    ax1.set_xlabel('Epoch')
-    ax1.set_ylabel('Binary Crossentropy')
-    ax1.legend()
-    ax1.grid(True)
+# def plot_history(history, fold):
+#     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+#     ax1.plot(history.history['loss'], label='Training Loss')
+#     ax1.plot(history.history['val_loss'], label='Validation Loss')
+#     ax1.set_xlabel('Epoch')
+#     ax1.set_ylabel('Binary Crossentropy')
+#     ax1.legend()
+#     ax1.grid(True)
 
-    ax2.plot(history.history['accuracy'], label='Training Accuracy')
-    ax2.plot(history.history['val_accuracy'], label='Validation Accuracy')
-    ax2.set_xlabel('Epoch')
-    ax2.set_ylabel('Accuracy')
-    ax2.legend()
-    ax2.grid(True)
+#     ax2.plot(history.history['accuracy'], label='Training Accuracy')
+#     ax2.plot(history.history['val_accuracy'], label='Validation Accuracy')
+#     ax2.set_xlabel('Epoch')
+#     ax2.set_ylabel('Accuracy')
+#     ax2.legend()
+#     ax2.grid(True)
 
-    fig.suptitle(f'Fold {fold + 1}')
-    plt.show()
+#     fig.suptitle(f'Fold {fold + 1}')
+#     plt.show()
 
-for fold, (train_index, val_index) in enumerate(cv.split(X, y)):
-    X_train, X_val = X[train_index], X[val_index]
-    y_train, y_val = y[train_index], y[val_index]
+# for fold, (train_index, val_index) in enumerate(cv.split(X, y)):
+#     X_train, X_val = X[train_index], X[val_index]
+#     y_train, y_val = y[train_index], y[val_index]
 
-    # Train the model
-    history = model.fit(X_train, y_train, epochs=100, batch_size=128, validation_data=(X_val, y_val), verbose=0)
+#     # Train the model
+#     history = model.fit(X_train, y_train, epochs=100, batch_size=128, validation_data=(X_val, y_val), verbose=0)
 
-    # Plot the training history for each fold
-    plot_history(history, fold)
+#     # Plot the training history for each fold
+#     plot_history(history, fold)
 
-# Evaluate the model on the entire test set
-test_preds = model.predict(X)
-test_preds_binary = np.round(test_preds).flatten()
-test_accuracy = accuracy_score(y, test_preds_binary)
-print('Test Accuracy:', test_accuracy)
+# # Evaluate the model on the entire test set
+# test_preds = model.predict(X)
+# test_preds_binary = np.round(test_preds).flatten()
+# test_accuracy = accuracy_score(y, test_preds_binary)
+# print('Test Accuracy:', test_accuracy)
