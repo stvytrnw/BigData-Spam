3174e46c9611be6be47f0535f66e020ddf542151
L3RiZ <107563807+stvytrnw@users.noreply.github.com>
2023-11-26 23:26:18 +0100

    wordcloud

--- Plots/DataAnalysis.py
+++ Plots/DataAnalysis.py
@@ -4,28 +4,37 @@ from wordcloud import WordCloud
 
 df = pd.read_csv("emails.csv")
 
-#Print dimensionality of the DataFrame
-print(df.shape)
+# Kopie des Datensatzes
+new_df = df.copy()
 
-#Print first 10 rows
-print(df.head(10))
+# Entfernen der ersten und letzten Spalte des Dataframes -> entsprechendes Format
+new_df.drop(['Email No.', 'Prediction'], inplace=True, axis=1)
 
-#Print last 10 rows
-print(df.tail(10))
+# Erfassen der Häufigkeit der Worte in der Mail
+new_df["transformed_text"] = new_df.apply(lambda row: " ".join(sum([[col] * row[col] for col in new_df.columns], [])), axis=1)
+df['transformed_text'] = new_df['transformed_text']
+print(
+    df[df['target'] == 1]['transformed_text'].str.cat(sep = "")
+)
 
-df = df[df.columns[1:]]
-
-print(' '.join(df[df['Prediction'] == 0]))
-
-#Plot a wordcloud for ham
-wordcloud = WordCloud(background_color='white').generate(' '.join(df[df['Prediction'] == 0]))
+# Plot a wordcloud for ham
+wordcloud = WordCloud(background_color='white').generate(df[df['target'] == 1]['transformed_text'].str.cat(sep = " "))
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
 
-#Plot a wordcloud for spam
-wordcloud = WordCloud(background_color='white').generate(' '.join(df[df['Prediction'] == 1]))
+# Plot a wordcloud for spam
+wordcloud = WordCloud(background_color='white').generate(df[df['target'] == 0]['transformed_text'].str.cat(sep = " "))
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
+
+# #Print dimensionality of the DataFrame
+# print(df.shape)
+
+# #Print first 10 rows
+# print(df.head(10))
+
+# #Print last 10 rows
+# print(df.tail(10))
 
